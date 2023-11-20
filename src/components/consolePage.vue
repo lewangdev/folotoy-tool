eaf6554d550ff5e0f38fcc2374d97a93a3920419
Chenyu Liu <liuchenyu@huitong-tech.com>
2023-11-20 15:08:18 +0800

    memorize command

--- src/components/consolePage.vue
+++ src/components/consolePage.vue
@@ -4,16 +4,15 @@
         <div id="terminal" style="position: relative;height: 500px; margin-top: 20px;">
         </div>
         <div style="width: 100%;display: flex;justify-content: flex-start;margin-top: -16px;">
-            <div
-            style="border: 0;background-color: #000;z-index: 1000;flex: 1">
-            <span style="color: #fff"> Folotoy $:</span>
-            <input type="text"
-                style="background-color: #000;color: #fff;width: 50%; border:none; text-indent: 10px;outline: none;z-index: 1000;"
-                autofocus @keydown="handleKeyDown">
+            <div style="border: 0;background-color: #000;z-index: 1000;flex: 1">
+                <span style="color: #fff"> Folotoy $:</span>
+                <input type="text"
+                    style="background-color: #000;color: #fff;width: 50%; border:none; text-indent: 10px;outline: none;z-index: 1000;"
+                    autofocus @keydown="handleKeyDown">
+            </div>
+            <div style="width: 15px;"></div>
         </div>
-        <div style="width: 15px;"></div>
-        </div>
-       
+
     </div>
 </template>
   
@@ -30,19 +29,37 @@ const props = defineProps({
 const onRestart = () => {
     props.deviceconsole.resetDevice()
 }
-const handleKeyDown = (event, ev) => {
-    console.log(event, ev)
+const commands = []
+let currentIndex = 0
+const handleKeyDown = (event) => {
     if (event.key === "Enter") {
         // 处理回车事件
+        commands.push(event.target.value)
+        currentIndex = commands.length - 1
         console.log("回车键已按下！");
         props.deviceconsole.term.writeTermDebouce('Folotoy $: ' + event.target.value + '\r\n')
         const encoder = new TextEncoder();
         const uint8Array = encoder.encode(event.target.value);
-        
+
         props.deviceconsole.write(uint8Array)
         console.log(uint8Array)
         event.target.value = ''
+    } else if (event.key === 'ArrowUp') {
+        if (commands.length === 0) return
+        if (currentIndex !== 0) {
+            event.target.value = commands[currentIndex--]
+        } else {
+            event.target.value = commands[0]
+        }
 
+    } else if (event.key === 'ArrowDown') {
+        if (commands.length === 0) return
+        if (currentIndex !== commands.length - 1) {
+            event.target.value = commands[currentIndex++]
+        } else {
+            event.target.value = commands[commands.length - 1]
+        }
+        console.log(currentIndex)
     }
 }
 </script>
@@ -70,5 +87,6 @@ const handleKeyDown = (event, ev) => {
 .xterm-cursor,
 .xterm-cursor-outline {
     display: none !important
-}</style>
+}
+</style>
   
